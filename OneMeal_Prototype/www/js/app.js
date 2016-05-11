9167e414a08e77fdb35093fae07cdb11cc618e11
janergorohhov <janer@ja.ee>
2016-05-12 01:21:43 +0300

    Geolocation + minor bug fixes

--- OneMeal_Prototype/www/js/app.js
+++ OneMeal_Prototype/www/js/app.js
@@ -1,18 +1,8 @@
-// Ionic Starter App
-
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
 var OneMeal = angular.module('starter', ['ionic', 'ngStorage', 'ngCordova', 'ion-datetime-picker'])
 var serverSideUrl = "http://onemeal.azurewebsites.net/";
 
 OneMeal.run(function ($ionicPlatform) {
     $ionicPlatform.ready(function() {
-        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-        // for form inputs)
-        //if(window.cordova && window.cordova.plugins.Keyboard) {
-            //cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-        //}
         if(window.StatusBar) {
             StatusBar.styleDefault();
         }
@@ -51,14 +41,6 @@ OneMeal.config(function ($stateProvider, $urlRouterProvider) {
             templateUrl: 'templates/history.html',
             controller: 'MyHistoryController'
         })
-    //$scope.logout = function () {
-    //    if ($localstorage.hasownproperty("accesstoken") === true && $localstorage.hasownproperty("userid") === true) {
-    //        $localstorage.clearall();
-    //    }
-    //    else {
-    //        alert("you are already logged out!")
-    //    }
-    //}
         .state('profile', {
             url: '/profile/:UserID/:MealID/:ProfileState',
             templateUrl: 'templates/profile.html',
@@ -68,7 +50,6 @@ OneMeal.config(function ($stateProvider, $urlRouterProvider) {
 });
 
 OneMeal.controller("LoginController", function ($scope, $cordovaOauth, $localStorage, $location, $http) {
-
     if ($localStorage.hasOwnProperty("accessToken") === true && $localStorage.hasOwnProperty("userId") === true)
         $location.path("/mainscreen");
     $scope.login = function() {
@@ -92,11 +73,11 @@ OneMeal.controller("LoginController", function ($scope, $cordovaOauth, $localSto
                         $location.path("/mainscreen");             
                     });
             }, function(error) {
-                alert("There was a problem getting your profile.  Check the logs for details.");
+                alert("There was a problem getting your profile.");
                 console.log(error);
             });         
         }, function(error) {
-            alert("There was a problem signing in!  See the console for logs");
+            alert("There was a problem signing in!");
             console.log(error);
         });
     };
@@ -124,11 +105,21 @@ OneMeal.controller("LogoutController", function ($scope, $localStorage, $locatio
 OneMeal.controller("MainScreenController", function ($scope, $http, $localStorage, $location) {
 
     $scope.init = function() {
-        if($localStorage.hasOwnProperty("accessToken") === true) {
+        if ($localStorage.hasOwnProperty("accessToken") === true) {
+            $scope.getCurrentPosition = function () {
+                function onSuccess(position) {
+                    $http.get(serverSideUrl + "api/activeprofiles/" + position.coords.latitude + "/" + position.coords.longitude + "/" + 0.5).then(function (response) {
+                        $scope.names = response.data;
+                    });
+                };
+                function onError(error) {
+                    alert("Please turn on your GPS");
+                };
+                navigator.geolocation.getCurrentPosition(onSuccess, onError, { enableHighAccuracy: true, maximumAge: 30000000, timeout: 30000 });
+            }
+            $scope.getCurrentPosition();
             $scope.showModal = false;
-            $http.get(serverSideUrl + "api/activeprofiles").then(function (response){
-                $scope.names = response.data;
-            });
+            
         } else {
             alert("Not signed in");
             $location.path("/login");
@@ -144,19 +135,34 @@ OneMeal.controller("DateTimePickerControl", function ($scope, $http, $localStora
 
     $scope.init = function () {
         if ($localStorage.hasOwnProperty("accessToken") === true) {
-            //$scope.datetimeValue = new date();
+
             var d = new Date()
-            $scope.location = "Choose a place to OneMeal";
-            $scope.datetimeValue = new Date(d.getFullYear(), d.getMonth(), d.getDate(), 12, 0, 0, 0);
-            $scope.OneMeal = function (_date, _location) {
+            $scope.chosen = {};
+            $scope.chosen.location = "";
+            $scope.chosen.datetimeValue = new Date(d.getFullYear(), d.getMonth(), d.getDate(), 12, 0, 0, 0);
+            $scope.OneMeal = function () {
                 // Writing it to the server
-                //		
-                var Data = {
-                    chosenDate: _date,
-                    userID: 4 //#TODO NB! For test purposes
-                };
-                var res = $http.post(serverSideUrl + 'api/activeprofiles', Data);
-                $location.path("/mainscreen");
+                //
+                if (new Date($scope.chosen.datetimeValue) < new Date())
+                {
+                    alert("Your OneMeal date is in the past. Please choose a new one");
+                    return;
+                }
+                if ($scope.chosen.location == "")
+                {
+                    alert("Please choose a location for your OneMeal");
+                    return;
+                }                   
+
+                    var Data = {
+                        chosenDate: $scope.chosen.datetimeValue,
+                        userID: 4, // NB! For test purposes,
+                        location: $scope.chosen.location
+
+                    };
+                    var res = $http.post(serverSideUrl + 'api/activeprofiles', Data);
+                    $location.path("/mainscreen");
+                
             };
 
         } else {
